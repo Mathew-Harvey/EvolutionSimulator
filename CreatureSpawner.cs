f5f6674946340ad243c7a5595205aadedbed9060
mathew harvey <mathewharvey@gmail.com>
2024-03-09 23:13:23 +0800

    refactor

--- CreatureSpawner.cs
+++ CreatureSpawner.cs
@@ -1,28 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureSpawner : MonoBehaviour
 {
-    public class CreatureSpawner : MonoBehaviour
+    public GameObject agentPrefab;
+    private GameObject[] agentList;
+    public int floorScale = 1;
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        public GameObject agentPrefab;
-        private GameObject[] agentList;
-        public int floorScale = 1
+        agentList = GameObject.FindGameObjectsWithTag("Agent");
 
-        // Update is called once per frame
-        void FixedUpdate()
+        // if there are no agents in the scene, spawn one at a random location. 
+        // This is to ensure that there is always at least one agent in the scene.
+        if (agentList.Length < 1)
         {
-            agentList = GameObject.FindGameObjectWithTag("Agent");
+            SpawnCreature();
+        } 
+    }
 
-            //if there are no agents in the scene, spawn one at a random location.
-            // This is to ensure that there is always at least one agent in the scene.
-            if (agentList.Length < 1)
-            {
-                SpawnCreature()
-            }
-        }
-
-        void SpawnCreature()
-        {
-            int x = Random.Range(-100, 101)*floorScale;
-            int z = Random.Range(-100, 101)*floorScale;
-            Instantiate(myPrefab, new Vector3((float)x, 0.75f, (float)z), Quaternion.identity);
-        }
+    void SpawnCreature()
+    {
+        int x = Random.Range(-100, 101)*floorScale;
+        int z = Random.Range(-100, 101)*floorScale;
+        Instantiate(agentPrefab, new Vector3((float)x, 0.75f, (float)z), Quaternion.identity);
     }
 }
